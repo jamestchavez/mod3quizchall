e5935222dc0a7a8fc63c40bfbce994478f780fbc
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-05-25 22:22:58 -0500

    updated READme a bit and added functions to script.js

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Module 4 Quiz Challenge
 Description:
 Create a timed quiz with questions pertaining to Javascript
 
+The user will access the website and will be greeted with the description of what they will be doing.
+    -Timer
+    -High Score Link
+    -Message about what is going to happen
+
+Once the user begins the quiz, the timer will count down to zero.
+
+Each wrong answer will penalize the user and remove 10 seconds from their timer.
--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//initial commit comment
+// variables
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+// Event listener for clearing scores
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Pulls local storage
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Event listener (move to index page)
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
